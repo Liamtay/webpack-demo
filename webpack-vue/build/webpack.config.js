a07126d3decc3ff8a43d4ce53c71e54ecc680c38
陈晓斌 <465120558@qq.com>
2020-08-02 11:15:56 +0800

    fix：2.5.1.2更正

--- webpack-vue/build/webpack.config.js
+++ webpack-vue/build/webpack.config.js
@@ -1,115 +1,115 @@
 const path = require('path')
-const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const vueLoaderPlugin = require('vue-loader/lib/plugin')
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const devMode = process.argv.indexOf('--mode=production') === -1;
 module.exports = {
-  entry:{
-    main:path.resolve(__dirname,'../src/main.js')
+  entry: {
+    main: path.resolve(__dirname, '../src/main.js')
   },
-  output:{
-    path:path.resolve(__dirname,'../dist'),
-    filename:'js/[name].[hash:8].js',
-    chunkFilename:'js/[name].[hash:8].js'
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'js/[name].[hash:8].js',
+    chunkFilename: 'js/[name].[hash:8].js'
   },
-  module:{
-    rules:[
+  module: {
+    rules: [
       {
-        test:/\.js$/,
-        use:{
-          loader:'babel-loader',
-          options:{
-            presets:['@babel/preset-env']
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
           }
         },
-        //exclude:/node_modules/
-      },
-      {
-        test:/\.vue$/,
-        use:[{
-          loader:'vue-loader',
-          options:{
-            compilerOptions:{
-              preserveWhitespace:false
-            }
-          },
-        }],
-        include:[path.resolve(__dirname,'../src')],
         exclude:/node_modules/
       },
       {
-        test:/\.css$/,
-        use:[{
-          loader: devMode ? 'vue-style-loader' : MiniCssExtractPlugin.loader,
-          options:{
-            publicPath:"../dist/css/",
-            hmr:devMode
-          }
-        },'css-loader',{
-          loader:'postcss-loader',
-          options:{
-            plugins:[require('autoprefixer')]
-          }
-        }]
-      },
-      {
-        test:/\.less$/,
-        use:[{
-          loader:devMode ? 'vue-style-loader' : MiniCssExtractPlugin.loader,
-          options:{
-            publicPath:"../dist/css/",
-            hmr:devMode
-          }
-        },'css-loader','less-loader',{
-          loader:'postcss-loader',
-          options:{
-            plugins:[require('autoprefixer')]
-          }
-        }]
-      },
-      {
-        test:/\.(jep?g|png|gif)$/,
-        use:{
-          loader:'url-loader',
-          options:{
-            limit:10240,
-            fallback:{
-              loader:'file-loader',
-              options:{
-                name:'img/[name].[hash:8].[ext]'
-              }
+        test: /\.vue$/,
+        use: [{
+          loader: 'vue-loader',
+          options: {
+            compilerOptions: {
+              preserveWhitespace: false
             }
           },
-           include:[path.resolve(__dirname,'../src/assets')],
-          // exclude:/node_modules/
-        }
+        }],
+        include: [path.resolve(__dirname, '../src')],
+        exclude: /node_modules/
       },
       {
-        test:/\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
-        use:{
-          loader:'url-loader',
-          options:{
-            limit:10240,
-            fallback:{
-              loader:'file-loader',
-              options:{
-                name:'media/[name].[hash:8].[ext]'
+        test: /\.css$/,
+        use: [{
+          loader: devMode ? 'vue-style-loader' : MiniCssExtractPlugin.loader,
+          options: {
+            publicPath: "../dist/css/",
+            hmr: devMode
+          }
+        }, 'css-loader', {
+          loader: 'postcss-loader',
+          options: {
+            plugins: [require('autoprefixer')]
+          }
+        }]
+      },
+      {
+        test: /\.less$/,
+        use: [{
+          loader: devMode ? 'vue-style-loader' : MiniCssExtractPlugin.loader,
+          options: {
+            publicPath: "../dist/css/",
+            hmr: devMode
+          }
+        }, 'css-loader', 'less-loader', {
+          loader: 'postcss-loader',
+          options: {
+            plugins: [require('autoprefixer')]
+          }
+        }]
+      },
+      {
+        test: /\.(jep?g|png|gif)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 10240,
+            fallback: {
+              loader: 'file-loader',
+              options: {
+                name: 'img/[name].[hash:8].[ext]'
+              }
+            },
+          },
+        },
+        include: [path.resolve(__dirname, '../src/assets')],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 10240,
+            fallback: {
+              loader: 'file-loader',
+              options: {
+                name: 'media/[name].[hash:8].[ext]'
               }
             }
           }
         }
       },
       {
-        test:/\.(woff2?|eot|ttf|otf)(\?.*)?$/i,
-        use:{
-          loader:'url-loader',
-          options:{
-            limit:10240,
-            fallback:{
-              loader:'file-loader',
-              options:{
-                name:'media/[name].[hash:8].[ext]'
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/i,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 10240,
+            fallback: {
+              loader: 'file-loader',
+              options: {
+                name: 'media/[name].[hash:8].[ext]'
               }
             }
           }
@@ -117,18 +117,18 @@ module.exports = {
       }
     ]
   },
-  resolve:{
-    alias:{
-      'vue$':'vue/dist/vue.runtime.esm.js',
-      ' @':path.resolve(__dirname,'../src'),
-      'assets':path.resolve(__dirname,'../src/assets')
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.runtime.esm.js',
+      ' @': path.resolve(__dirname, '../src'),
+      'assets': path.resolve(__dirname, '../src/assets')
     },
-    extensions:['*','.js','.json','.vue']
+    extensions: ['*', '.js', '.json', '.vue']
   },
-  plugins:[
+  plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template:path.resolve(__dirname,'../public/index.html')
+      template: path.resolve(__dirname, '../public/index.html')
     }),
     new vueLoaderPlugin(),
     new MiniCssExtractPlugin({
